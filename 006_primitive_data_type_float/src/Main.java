1d1a183227cda9f013f4aeda60be07661c5c9356
ak <devworksatul@gmail.com>
2026-03-03 13:16:43 +0530

    done

--- 006_primitive_data_type_float/src/Main.java
+++ 006_primitive_data_type_float/src/Main.java
@@ -17,5 +17,59 @@ void main() {
 
     System.out.println(x);
 
+    boolean var=true;
+    System.out.println(var);
+
+    char a='\u00A7';
+    System.out.println(a);
+
+
+    String s1="this is atul string";
+    String s2=" this is another string";
+    System.out.println(s1+s2);
+
+    short shortValue=12;
+    byte bytValue=(byte)shortValue;
+    System.out.println(bytValue);
+
+
+    //arithmetic operators
+
+
+    int xxx=3;
+    switch (xxx){
+        case 1-> System.out.println("hello "+1);
+        case 2-> System.out.println("hii "+2);
+        case 3-> System.out.println("bye "+3);
+        default -> System.out.println("tata");
+    }
+
+    int arr[]={1,2,3,4,5};
+    for(int xy:arr){
+        System.out.println(xy);
+    }
+
+    //sum of digits
+
+    int digit=1284848;
+
+
+    int sumDigit=0;
+    System.out.println("***************");
+    do{
+        System.out.println(sumDigit+" - "+digit);
+        sumDigit+=digit%10;
+        digit=digit/10;
+
+    }
+    while(digit>0);
+
+    System.out.println("*************");
+    System.out.println(sumDigit);
+    System.out.println("*****************");
+    System.out.println(0b010110101010);
+
 
 }
+
+
